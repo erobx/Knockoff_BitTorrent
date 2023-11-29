31ff2d25f66b676612640d0db92d4b39da856d80
VBoudreau55 <vancebou55@gmail.com>
2023-11-28 22:56:09 -0500

    piece payload func and send message readability

--- src/messages/Message.java
+++ src/messages/Message.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.http.HttpClient.Redirect;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 import javax.swing.plaf.PanelUI;
 
@@ -75,16 +76,16 @@ public abstract class Message implements Serializable {
         // DataOutputStream dataOutputStream = new DataOutputStream(out);
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(out));
 
-        //dataOutputStream.writeInt(length);
+        // dataOutputStream.writeInt(length);
         ByteBuffer buffer = ByteBuffer.allocate(1 + 4 + payload.length);
         buffer.putInt(length);
-        //dataOutputStream.writeByte(type);
+        // dataOutputStream.writeByte(type);
         buffer.put(type);
         if (payload != null) {
-            //dataOutputStream.write(payload);
+            // dataOutputStream.write(payload);
             buffer.put(payload);
         }
-        //dataOutputStream.flush();
+        // dataOutputStream.flush();
         Charset charset = Charset.forName("US-ASCII");
         System.out.println("Serialized message" + charset.decode(buffer).toString());
         output.write(charset.decode(buffer).toString());
@@ -118,6 +119,34 @@ public abstract class Message implements Serializable {
 
     public int getType() {
         return (int) type;
+    }
+
+    public static byte[] createPiecePayload(int pieceIndex, byte[] piece) {
+        // Assuming pieceIndex is a 4-byte integer (32 bits)
+        byte[] indexBytes = new byte[4];
+        indexBytes[0] = (byte) (pieceIndex >> 24);
+        indexBytes[1] = (byte) (pieceIndex >> 16);
+        indexBytes[2] = (byte) (pieceIndex >> 8);
+        indexBytes[3] = (byte) pieceIndex;
+
+        // Combine the index and the piece data
+        byte[] payload = new byte[indexBytes.length + piece.length];
+        System.arraycopy(indexBytes, 0, payload, 0, indexBytes.length);
+        System.arraycopy(piece, 0, payload, indexBytes.length, piece.length);
+
+        return payload;
+    }
+
+    public static Pair<Integer, byte[]> decodePiecePayload(byte[] payload) {
+        // Extract the piece index from the first 4 bytes of the payload
+        int pieceIndex = (payload[0] & 0xFF) << 24 | (payload[1] & 0xFF) << 16 |
+                (payload[2] & 0xFF) << 8 | (payload[3] & 0xFF);
+
+        // Extract the piece data from the rest of the payload
+        byte[] pieceData = new byte[payload.length - 4];
+        System.arraycopy(payload, 4, pieceData, 0, pieceData.length);
+
+        return new Pair<>(pieceIndex, pieceData);
     }
 
     // public int getIndex() {
@@ -187,77 +216,91 @@ public abstract class Message implements Serializable {
             throw new RuntimeException("Client receiver ID Cannot be found");
         }
 
+        Message msg = null;
+        String msgType = "";
+
         // TODO check if parameters are correct
         switch (type) {
             // Choke - no payload
             case CHOKE -> {
                 int length = 0; // no payload
-                Message msg = new MsgChoke(length + 1, (byte) type.getValue(), null, senderID,
+                msg = new MsgChoke(length + 1, (byte) type.getValue(), null, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("CHOKE message sent from " + senderID + " to " + receiverID);
             }
             // Unchoke - no payload
             case UNCHOKE -> {
                 int length = 0; // no payload
-                Message msg = new MsgUnchoke(length + 1, (byte) type.getValue(), null, senderID,
+                msg = new MsgUnchoke(length + 1, (byte) type.getValue(), null, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("UNCHOKE message sent from " + senderID + " to " + receiverID);
             }
             // Interested - no payload
             case INTERESTED -> {
                 int length = 0; // no payload
-                Message msg = new MsgInt(length + 1, (byte) type.getValue(), null, senderID,
+                msg = new MsgInt(length + 1, (byte) type.getValue(), null, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("INTERESTED message sent from " + senderID + " to " + receiverID);
             }
             // Not interested - no payload
             case NOT_INTERESTED -> {
                 int length = 0; // no payload
-                Message msg = new MsgNotInt(length + 1, (byte) type.getValue(), null, senderID,
+                msg = new MsgNotInt(length + 1, (byte) type.getValue(), null, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("NOT_INTERESTED message sent from " + senderID + " to " + receiverID);
             }
             // Have - index payload TODO adjust payload
             case HAVE -> {
                 int length = 0; // no payload
-                Message msg = new MsgHave(length + 1, (byte) type.getValue(), null, senderID,
+                msg = new MsgHave(length + 1, (byte) type.getValue(), null, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("HAVE message sent from " + senderID + " to " + receiverID);
             }
             // Bitfield - bitfield payload
             case BITFIELD -> {
                 int length = payload.length;
-                Message msg = new MsgBitfield(length + 1, (byte) type.getValue(), payload, senderID,
+                msg = new MsgBitfield(length + 1, (byte) type.getValue(), payload, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("BITFIELD message sent from " + senderID + " to " + receiverID);
             }
             // Request - index payload TODO adjust payload and length
             case REQUEST -> {
                 int length = payload.length;
-                Message msg = new MsgRequest(length + 1, (byte) type.getValue(), payload, senderID,
+                msg = new MsgRequest(length + 1, (byte) type.getValue(), payload, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("REQUEST message sent from " + senderID + " to " + receiverID);
             }
             // Piece - index + piece payload TODO adjust payload and length
             case PIECE -> {
                 int length = payload.length;
-                Message msg = new MsgPiece(length + 1, (byte) type.getValue(), payload, senderID,
+                msg = new MsgPiece(length + 1, (byte) type.getValue(), payload, senderID,
                         receiverID);
-                msg.serialize(ch.getSocket().getOutputStream());
-                System.out.println("PIECE message sent from " + senderID + " to " + receiverID);
             }
+        }
+
+        // send msg if appropriate type is inputted
+        if (msg != null) {
+            msg.serialize(ch.getSocket().getOutputStream());
+            System.out.println(type.toString() + " message sent from " + senderID + " to " + receiverID);
         }
 
     }
 
     public void handle() throws Exception {
         throw new Exception("Handler not overriden default handler called");
+    }
+
+    public byte[] loadPiece(int pieceIndex, int senderID) {
+        // parses the specified piece into the byte array to be sent.
+
+        String fileLocation = "/src/peer_" + senderID + "/" + pieceIndex;
+        Scanner scan;
+        try {
+            scan = new Scanner(new File(fileLocation));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Cannot find file: " + fileLocation, e);
+        }
+        StringBuilder pieceBuffer = new StringBuilder();
+        while (scan.hasNext()) {
+            pieceBuffer.append(scan.next());
+        }
+        byte[] piece = pieceBuffer.toString().getBytes();
+
+        scan.close();
+
+        return piece;
     }
 }
